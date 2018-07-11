ec647854877c6b3e82a473031e614f03c14fcefc
Felipe Jornada <fjornada@gmail.com>
2018-07-11 15:21:28 -0700

    Correct upstream

--- update-config.sh
+++ update-config.sh
@@ -5,5 +5,5 @@
 #
 # Felipe H. da Jornada (2018)
 
-git config alias.update-config '!cd $(git rev-parse --show-toplevel) && curl -LsS -o .git/bgw-config https://raw.githubusercontent.com/BerkeleyGW/BerkeleyGW-git-config/master/bgw-config && git config include.path bgw-config && echo "Configuration updated" || echo "Update failed"'
+git config alias.update-config '!cd $(git rev-parse --show-toplevel) && curl -LsS -o .git/bgw-config https://raw.githubusercontent.com/BerkeleyGW/BerkeleyGW-git-config/master/bgw-config && git config include.path bgw-config && echo "Configuration updated" || echo "Update failed"' && echo 'Configuration installed' || echo 'Configuration script could not be installed'
 git update-config
